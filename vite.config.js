5a41978b1602295eaa2ee529035ef76fe372d739
AdamMannino <amannino92@gmail.com>
2026-01-02 23:25:50 -0500

    Deployment Config Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // ⬇️ IMPORTANT: change this to your repo name
+  base: "/wings_public_calendar/",
+});
