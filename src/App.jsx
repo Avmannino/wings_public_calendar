14dbf735dd870c9cfb154055f8a570b633d53f06
Adam Mannino <amannino92@gmail.com>
2026-05-26 15:38:57 -0400

    Display Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
 import WingsCalendar from "./components/WingsCalendar";
 import "./calendar.css";
 
 export default function App() {
-  useEffect(() => {
-    console.log("APP IS RUNNING - GITHUB PAGES TEST");
-  }, []);
-
   return (
     <div className="page">
       <h1>THIS WEEK'S LINEUP</h1>
